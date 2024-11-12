31599a47feea7bd50c97367029d2dc7f47b87785
GasanovaFerida <gasanovaferida7@gmail.com>
2024-11-12 23:41:15 +0300

    Add files via upload

---
+++ задание 1.py
@@ -0,0 +1,9 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+missing_index = numbers.index(None)
+
+sum_numbers = sum(numbers[:missing_index] + numbers[missing_index+1:])
+count_numbers = len(numbers)
+
+average = sum_numbers / count_numbers
+numbers[missing_index] = average
+print("Измененный список:",numbers)
